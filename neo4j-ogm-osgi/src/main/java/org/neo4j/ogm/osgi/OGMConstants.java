d3fad40ce6b4eb36a17bdc07300681fe487e9636
gurkerl83 <markus_gritsch@gmx.de>
2020-06-11 12:42:00 +0200

    Improve session factory concept
    Provide the neo4j-ogm session factory service through an OSGi component and factory. Previously only the factory variant was provided due to a duplication of a single PID on both options.
    
    Increase dependency version of neo4j-java-driver
    
    The latest development version of neo4j-java-driver was made available, increase version from 4.0 to 4.1.

--- neo4j-ogm-osgi/src/main/java/org/neo4j/ogm/osgi/OGMConstants.java
+++ neo4j-ogm-osgi/src/main/java/org/neo4j/ogm/osgi/OGMConstants.java
@@ -1,5 +1,6 @@
 package org.neo4j.ogm.osgi;
 
 public class OGMConstants {
-  public static final String CONFIGURATION_PID = "ogm.session.component";
+  public static final String OGM_SESSION_COMPONENT_CONFIG = "ogm.session.component.pid";
+  public static final String OGM_SESSION_FACTORY_CONFIG = "ogm.session.factory.pid";
 }
